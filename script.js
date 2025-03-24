7bf626ed9dbeddeba36176d0550517f0023d4408
Alpha-Zero022 <aks222tritwo@gmail.com>
2025-03-24 08:20:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var txt;
+  if (confirm("Please press OK button, if those are helpful. If it is not like that, please press Cancle button.")) {
+    txt = "So, these are useful.";
+  } else {
+    txt = "So, these are not useful.";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
