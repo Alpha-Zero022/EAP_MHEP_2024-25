251dac19b31cfb63a6e1eef532512254dcd3e9a4
Alpha-Zero022 <aks222tritwo@gmail.com>
2025-03-24 13:51:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+alert('Welcome to EAP! I hope you wiil find some useful knowledge.');
 function myFunction() {
   var txt;
   if (confirm("Please press OK button, if those are helpful. If it is not like that, please press Cancle button.")) {
